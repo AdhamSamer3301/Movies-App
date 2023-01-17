be5131937370532257292eddf5049d814e63a998
Adham Samer <adhamsamer3301@gmail.com>
2023-01-17 13:37:49 +0200

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Adham Samer on 17/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
